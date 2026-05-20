4f8d2d5e8e9956e0f7ff1965ce5b90fc113401af
Mohammed_Sabry <mohammedsabry3413@gmail.com>
2026-05-20 14:19:59 +0300

    LICENSE

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+  start License Header
+  This template and related code are licensed under the MIT License.
+  Credit is required: github.com/Mohammedsabry13
+  Author: Mohammed Sabry
+  end License Header
+*/
+
+// If the project needs shared JS, place it here.
+// Currently, index.html already includes inline scripts for:
+// - toggleMenu()
+// - carousel circle indicators
+
